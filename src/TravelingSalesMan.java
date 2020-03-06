5e4e1140d59c1ec74ae05b042b469ffed15476ff
gaberife <gabrielle04rife@gmail.com>
2020-03-06 09:54:40 -0500

    Program initializes array with N number of cities.

--- src/TravelingSalesMan.java
+++ src/TravelingSalesMan.java
@@ -2,7 +2,8 @@ import java.util.*;
 import java.io.*;
 import java.io.File;
 
-public class TravelingSalesMan {
+public class TravelingSalesMan{
+
     public static void main(String[] args) {
         System.out.println("Welcome user, its time to solve a maze. Please enter the file path.");
         Scanner input = new Scanner(System.in);
@@ -11,12 +12,14 @@ public class TravelingSalesMan {
         try {
             confirmedFile = new File(String.valueOf(check(fileName)));
             // Variable that represents the file path
-            //init(confirmedFile);
+            init(confirmedFile);
             //Initializes the data
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
+
     public static File check(String fileName) throws IOException {
         File file = new File(fileName);
         //Creates instance of file object
@@ -37,43 +40,19 @@ public class TravelingSalesMan {
         }
         return file;
     }
-/*
+
     public static void init(File file) throws IOException {
-        Maze theMaze = new Maze();
         BufferedReader line = new BufferedReader(new FileReader(file));
+        //Isolates the String first line which shows N number of cities
         String ch = line.readLine();
-        //Isolates the first line which shows the values of rows and columns
-        String[] splt = ch.split(" ", 0);
-        //Supposed to split the two string values into a string array
-        numCol = Integer.parseInt(splt[1]);
-        numRow = Integer.parseInt(splt[0]);
-        //Assigns appropriate Column and Row values based on whichever maze file is called
+        Citys.NumCities = Integer.parseInt(ch);
+        Random random = new Random();
+        Citys.initCities(Citys.NumCities);//allocating memory to array
 
-        theMaze.maze = new char[numCol][numRow];
-        for (int y = 0; y < numRow; y++) {
-            String test = line.readLine();
-            for (int x = 0; x < test.length(); x++) {
-                theMaze.maze[x][y] = test.charAt(x);
-            }
+        for(int i = 0; i < Citys.NumCities; i++){
+            int randPop = random.nextInt(Citys.NumCities);
+            Citys.pop[i][randPop] = (randPop);
+            System.out.println(Citys.pop[1][randPop]);
         }
-        //To assign data from the file to each dimension of the array
-        //2d-array intended to represent the maze/
-
-        //Finds the start Position and places marker there
-        for (int y = 0; y < numRow; y++) {
-            if (' ' == (theMaze.maze[0][y])) {
-                theMaze.start = new Position(0, y);
-            }
-        }
-        //Finds the end Position
-        for (int y = 0; y < numRow; y++) {
-            if (' ' == theMaze.maze[numCol - 1][y]) {
-                theMaze.end = new Position(numCol - 1, y);
-            }
-        }
-        mazeSolver(theMaze);
     }
-
- */
-
 }
